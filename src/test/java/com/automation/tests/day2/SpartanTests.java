dc18597d9950b649459527c4e0ff80b9a7c690c1
Like Sha <tradersalike88@yahoo.com>
2020-05-17 14:00:05 -0700

    Added File code

--- src/test/java/com/automation/tests/day2/SpartanTests.java
+++ src/test/java/com/automation/tests/day2/SpartanTests.java
@@ -4,6 +4,8 @@ import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static io.restassured.RestAssured.given;
 
 public class SpartanTests {
@@ -35,8 +37,13 @@ public class SpartanTests {
     public void addSpartan(){
         //JSON supports different data types: string, integer, boolean
         String body = "{\"gender\": \"Male\", \"name\": \"Random User\", \"phone\": 9999999999}";
+        //instead of string variable, we can use external JSON file
+        //use File class to read JSON and pass it into body
+        //provide path to the JSON as a parameter
+        File jsonFile = new File(System.getProperty("user.dir") + "/spartan.json");
 
         //to create new item, we perform POST request
+        //contentType(ContentType.JSON) - to tell web service what kind of media type we send
         given().
                 contentType(ContentType.JSON).
                 auth().basic("admin", "admin").
